0747115c2b89e5be27e083a3ab8a941b2ecc7dae
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2020-12-26 14:31:32 +0000

    init animal blazor project
    init animal blazor project

---
+++ AnimalDeCompagnieNoSuBlazor/Models/AnimalRawdata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalDeCompagnieNoSuBlazor.Models
+{
+    public class AnimalRawdata
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public double Age { get; set; }
+        public string ShortDescribe { get; set; }
+    }
+}
---
+++ AnimalDeCompagnieNoSuBlazor/Pages/Animal.razor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AnimalDeCompagnieNoSuBlazor.Models;
+using Microsoft.AspNetCore.Components;
+using AntDesign;
+using System.Net.Cache;
+
+namespace AnimalDeCompagnieNoSuBlazor.Pages
+{
+    public partial class Animal
+    {
+        private readonly ListGridType _listGridType = new ListGridType
+        {
+            Gutter = 24,
+            Column = 4
+        };
+
+        private AnimalRawdata[] _data = { };
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            var list = new List<AnimalRawdata>();
+            for (int i = 0; i < 20; i++)
+            {
+                var data = new AnimalRawdata()
+                {
+                    Id = i,
+                    Name = "this is name " + i,
+                    Age = i % 9,
+                    ShortDescribe = "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+                };
+                list.Add(data);
+            }
+            _data = list.ToArray();
+        }
+    }
+}
