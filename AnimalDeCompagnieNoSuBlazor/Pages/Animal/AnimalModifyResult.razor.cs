ece0b645fbb8a3ebc53e622d7df3b6e4e8e2feb4
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-01-03 14:27:52 +0000

    add result page
    add result page

---
+++ AnimalDeCompagnieNoSuBlazor/Pages/Animal/AnimalModifyResult.razor.cs
@@ -0,0 +1,15 @@
+﻿using AnimalDeCompagnieNoSuBlazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using AntDesign;
+
+namespace AnimalDeCompagnieNoSuBlazor.Pages.Animal
+{
+    public partial class AnimalModifyResult
+    {
+        private readonly AnimalUpdateModel AnimalUpdateModel = new();
+    }
+}
