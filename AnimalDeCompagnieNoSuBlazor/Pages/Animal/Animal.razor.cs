e6a526f6bcf99edad8777467be82c65cb08fe4d7
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-01-23 17:27:42 +0000

    add search for animal list
    add search for animal list

--- AnimalDeCompagnieNoSuBlazor/Pages/Animal/Animal.razor.cs
+++ AnimalDeCompagnieNoSuBlazor/Pages/Animal/Animal.razor.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Components;
 using AntDesign;
 using System;
 using AnimalDeCompagnieNoSuBlazor.Services;
+using System.Text.Json.Serialization;
+using System.Linq;
 
 namespace AnimalDeCompagnieNoSuBlazor.Pages.Animal
 {
@@ -16,13 +18,51 @@ namespace AnimalDeCompagnieNoSuBlazor.Pages.Animal
             Column = 4
         };
 
+        private readonly List<SelectType> _typeItems = new()
+        {
+            new SelectType { Text = "cat", Value = "cat" },
+            new SelectType { Text = "dog", Value = "dog" },
+            new SelectType { Text = "fish", Value = "fish" },
+            new SelectType { Text = "dark", Value = "dark" }
+        };
+        private readonly List<SelectType> _truefalseItems = new()
+        {
+            new SelectType { Text = "是", Value = "0" },
+            new SelectType { Text = "否", Value = "1" }
+        };
+
+
+        private IEnumerable<string> _selectedTypeValues;
+        private void OnSelectedTypesChangedHandler(IEnumerable<SelectType> values)
+        {
+            //TODO :filter AnimalList
+        }
+
+        private IEnumerable<string> _selectedSterilizationValues;
+        private void OnSelectedSterilizationChangedHandler(IEnumerable<SelectType> values)
+        {
+            //TODO :filter AnimalList
+        }
+
+        private IEnumerable<string> _selectedVaccinValues;
+        private void OnSelectedVaccinChangedHandler(IEnumerable<SelectType> values)
+        {
+            //TODO :filter AnimalList
+        }
+
         private AnimalListViewModel[] _data = { };
         [Inject] private IAnimalService AnimalService { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
             var list = await AnimalService.GetAnimalList();
             _data = list.ToArray();
         }
+    }
+    public class SelectType
+    {
+        public string Value { get; set; }
+        public string Text { get; set; }
     }
 }
