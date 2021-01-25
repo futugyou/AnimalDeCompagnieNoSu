e3d92dc71d84654a25c8d9f30ae5457d74b2f70b
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-01-25 12:48:45 +0000

    Update get.rs
    use Option

--- AnimalCenter/src/animal/get.rs
+++ AnimalCenter/src/animal/get.rs
@@ -1,8 +1,10 @@
 use crate::animal::animal::Animal;
 use actix_web::{web, HttpRequest, HttpResponse};
-pub async fn animal_handler(item: web::Json<Animal>, req: HttpRequest) -> HttpResponse {
+pub async fn animal_handler(item: Option<web::Json<Animal>>, req: HttpRequest) -> HttpResponse {
     println!("request: {:?}", req);
     println!("model: {:?}", item);
-
-    HttpResponse::Ok().json(item.0) // <- send json response
+    match item {
+        None => HttpResponse::Ok().json("ok"),
+        Some(i) => HttpResponse::Ok().json(i.0),
+    }
 }
