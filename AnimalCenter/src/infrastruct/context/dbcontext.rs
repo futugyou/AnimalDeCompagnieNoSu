5327ed379322d26b1c2d07e695809b14f8336707
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-01-26 17:29:46 +0000

    Update dbcontext.rs
    remove mutex

--- AnimalCenter/src/infrastruct/context/dbcontext.rs
+++ AnimalCenter/src/infrastruct/context/dbcontext.rs
@@ -1,20 +1,19 @@
 use crate::infrastruct::config::{Config, IConfig};
 use async_trait::async_trait;
 use mongodb::{options::ClientOptions, Client};
-use std::sync::Mutex;
 #[async_trait]
 pub trait IDbContext {
-    async fn get_db_context(&self) -> Mutex<Client>;
+    async fn get_db_context(&self) -> Client;
 }
 
 pub struct DBContext {}
 
 #[async_trait]
 impl IDbContext for DBContext {
-    async fn get_db_context(&self) -> Mutex<Client> {
+    async fn get_db_context(&self) -> Client {
         let _config = Config {};
         let conn_str = _config.get_config_with_key("MONGODB_URI");
         let client_options = ClientOptions::parse(&conn_str[..]).await.unwrap();
-        Mutex::new(Client::with_options(client_options).unwrap())
+        Client::with_options(client_options).unwrap()
     }
 }
