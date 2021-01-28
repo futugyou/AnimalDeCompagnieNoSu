352f69234ffc7908dee99465462c719f446ca17c
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-01-28 11:45:47 +0000

    Update post.rs
    add more filed for animal update request

--- AnimalCenter/src/animal/post.rs
+++ AnimalCenter/src/animal/post.rs
@@ -1,9 +1,29 @@
-use crate::animal::animal::Animal;
 use crate::infrastruct::context::dbcontext::{DBContext, IDbContext};
-use actix_web::{web, HttpRequest, HttpResponse};
-use mongodb::bson::doc;
+use crate::infrastruct::date_format;
 
-pub async fn animal_handler(item: Option<web::Json<Animal>>, req: HttpRequest) -> HttpResponse {
+use actix_web::{web, HttpRequest, HttpResponse};
+use chrono::{DateTime, Utc};
+use mongodb::bson::doc;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AnimalUpdateRequest {
+    #[serde(default)]
+    pub id: String,
+    #[serde(default)]
+    pub name: String,
+    #[serde(default)]
+    #[serde(rename = "type")]
+    pub animal_type: String,
+    #[serde(default)]
+    pub sub_type: String,
+    #[serde(with = "date_format")]
+    pub birthday: DateTime<Utc>,
+}
+pub async fn animal_handler(
+    item: Option<web::Json<AnimalUpdateRequest>>,
+    req: HttpRequest,
+) -> HttpResponse {
     println!("request: {:?}", req.path());
     println!("model: {:?}", item);
     match item {
