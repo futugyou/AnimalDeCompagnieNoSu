750c5e5b481e485a5e0d307e6aae2a0ccd5a8d11
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-02-03 17:14:32 +0000

    Update animalrepository.rs
    add update for animal repositroy

--- AnimalCenter/src/repository/animalrepository.rs
+++ AnimalCenter/src/repository/animalrepository.rs
@@ -15,16 +15,24 @@ pub trait IAnimalRepository {
     async fn update(&self, entity: AnimalEntity) -> Result<bool, String>;
 }
 
-pub struct AnimalRepository {}
+pub struct AnimalRepository {
+    collection: mongodb::Collection,
+}
 
-#[async_trait]
-impl IAnimalRepository for AnimalRepository {
-    async fn add(&self, entity: AnimalEntity) -> String {
+impl AnimalRepository {
+    pub async fn new() -> Self {
         let dbcontext = DBContext {};
         let dbclient = dbcontext.get_db_context().await;
         let collection = dbclient
             .database("react-app")
             .collection(AnimalEntity::get_collection_name());
+        Self { collection }
+    }
+}
+
+#[async_trait]
+impl IAnimalRepository for AnimalRepository {
+    async fn add(&self, entity: AnimalEntity) -> String {
         let docs = doc! {
                 "name": entity.name,
                 "type": entity.animal_type,
@@ -32,7 +40,7 @@ impl IAnimalRepository for AnimalRepository {
                 "sub_type":entity.sub_type,
                 "idcard":entity.idcard,
         };
-        let result = collection.insert_one(docs, None).await;
+        let result = self.collection.insert_one(docs, None).await;
         match result {
             Ok(r) => r.inserted_id.to_string(),
             Err(e) => {
@@ -43,7 +51,20 @@ impl IAnimalRepository for AnimalRepository {
     }
 
     async fn update(&self, entity: AnimalEntity) -> Result<bool, String> {
-        todo!()
+        let filter = doc! {"_id":entity.id};
+        let update = doc! {"$set" : doc!{
+                "name": entity.name,
+                "type": entity.animal_type,
+                "birthday":entity.birthday,
+                "sub_type":entity.sub_type,
+                "idcard":entity.idcard,
+        }};
+
+        let result = self.collection.update_one(filter, update, None).await;
+        match result {
+            Ok(r) => Ok(true),
+            Err(e) => Err(e.to_string()),
+        }
     }
 
     async fn delete(&self, entity: AnimalEntity) -> bool {
