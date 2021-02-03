fc35258e5c995e1f45ce981cca7952e3ee0ebf60
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-02-03 16:16:11 +0000

    Update animalrepository.rs
    impl insert for animalrepository

--- AnimalCenter/src/repository/animalrepository.rs
+++ AnimalCenter/src/repository/animalrepository.rs
@@ -1,10 +1,14 @@
-use crate::entity::animalentity::AnimalEntity;
+use crate::{
+    entity::animalentity::AnimalEntity,
+    infrastruct::context::dbcontext::{DBContext, IDbContext},
+};
 
 use async_trait::async_trait;
+use bson::doc;
 
 #[async_trait]
 pub trait IAnimalRepository {
-    async fn add(&self, entity: AnimalEntity) -> bool;
+    async fn add(&self, entity: AnimalEntity) -> String;
     async fn delete(&self, entity: AnimalEntity) -> bool;
     async fn findone<T>(&self, con: fn(t: T) -> bool) -> AnimalEntity;
     async fn findmany<T>(&self, con: fn(t: T) -> bool) -> Vec<AnimalEntity>;
@@ -15,8 +19,27 @@ pub struct AnimalRepository {}
 
 #[async_trait]
 impl IAnimalRepository for AnimalRepository {
-    async fn add(&self, entity: AnimalEntity) -> bool {
-        todo!()
+    async fn add(&self, entity: AnimalEntity) -> String {
+        let dbcontext = DBContext {};
+        let dbclient = dbcontext.get_db_context().await;
+        let collection = dbclient
+            .database("react-app")
+            .collection(AnimalEntity::get_collection_name());
+        let docs = doc! {
+                "name": entity.name,
+                "type": entity.animal_type,
+                "birthday":entity.birthday,
+                "sub_type":entity.sub_type,
+                "idcard":entity.idcard,
+        };
+        let result = collection.insert_one(docs, None).await;
+        match result {
+            Ok(r) => r.inserted_id.to_string(),
+            Err(e) => {
+                println!("{:?}", e);
+                "".to_string()
+            }
+        }
     }
 
     async fn update(&self, entity: AnimalEntity) -> Result<bool, String> {
