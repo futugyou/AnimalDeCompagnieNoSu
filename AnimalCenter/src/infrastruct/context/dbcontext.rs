bf7c721fb1d9cc6a2774c0cfba46f9478a99d27a
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-02-11 16:12:55 +0000

    Update dbcontext.rs
    use as_str() to get &str
    next,change client to Result<> type

--- AnimalCenter/src/infrastruct/context/dbcontext.rs
+++ AnimalCenter/src/infrastruct/context/dbcontext.rs
@@ -13,7 +13,7 @@ impl IDbContext for DBContext {
     async fn get_db_context(&self) -> Client {
         let _config = Config::new();
         let conn_str = _config.mongodb_uri;
-        let client_options = ClientOptions::parse(&conn_str[..]).await.unwrap();
+        let client_options = ClientOptions::parse(conn_str.as_str()).await.unwrap();
         Client::with_options(client_options).unwrap()
     }
 }
