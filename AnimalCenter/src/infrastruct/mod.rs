5c9dc6ad272221f99535a30e8c8dafbed05a08cd
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-02-13 17:02:31 +0000

    Update mod.rs
    add string to objectid function

--- AnimalCenter/src/infrastruct/mod.rs
+++ AnimalCenter/src/infrastruct/mod.rs
@@ -1,4 +1,5 @@
 use chrono::{DateTime, TimeZone, Utc};
+use custom_error::CustomError;
 
 pub mod config;
 pub mod context;
@@ -11,4 +12,8 @@ pub const DATETIMEDEFAULT: &'static str = "1900-01-01T01:01:01Z";
 
 pub fn getdefaultdatetime() -> DateTime<Utc> {
     Utc.datetime_from_str(DATETIMEDEFAULT, FORMAT).unwrap()
+}
+
+pub fn stringtoObjectId(str: &str) -> Result<bson::oid::ObjectId, CustomError> {
+    Ok(bson::oid::ObjectId::with_string(str)?)
 }
