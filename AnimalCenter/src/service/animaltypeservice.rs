61a98681aa863b1c63b0b4438d7038be9464c97f
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-02-18 14:23:37 +0000

    Update animaltypeservice.rs
    fill  search_animal_types service

--- AnimalCenter/src/service/animaltypeservice.rs
+++ AnimalCenter/src/service/animaltypeservice.rs
@@ -1,8 +1,11 @@
 use async_trait::async_trait;
 
-use crate::model::animal::animaltypemodel::{
-    AnimalTypeSearchRequest, AnimalTypeSearchResponse, AnimalTypeUpdateRequest,
-    AnimalTypeUpdateResponse,
+use crate::{
+    model::animal::animaltypemodel::{
+        AnimalTypeSearchRequest, AnimalTypeSearchResponse, AnimalTypeUpdateRequest,
+        AnimalTypeUpdateResponse,
+    },
+    repository::animaltyperepository::{AnimalTypeRepository, IAnimalTypeRepository},
 };
 
 #[async_trait]
@@ -17,15 +20,40 @@ pub trait IAnimalTypeService {
     ) -> AnimalTypeUpdateResponse;
 }
 
-pub struct AnimalTypeService {}
+pub struct AnimalTypeService {
+    animaltype_repository: Box<dyn IAnimalTypeRepository + Send + Sync>,
+}
+
+impl AnimalTypeService {
+    pub async fn new() -> Self {
+        let animaltype_repository = AnimalTypeRepository::new().await;
+        Self {
+            animaltype_repository: Box::new(animaltype_repository),
+        }
+    }
+}
 
 #[async_trait]
 impl IAnimalTypeService for AnimalTypeService {
+    #[tracing::instrument(skip(self))]
     async fn search_animal_types(
         &self,
-        _request: AnimalTypeSearchRequest,
+        request: AnimalTypeSearchRequest,
     ) -> Vec<AnimalTypeSearchResponse> {
-        todo!()
+        let doc = request.into();
+        let result = self.animaltype_repository.findmany(doc).await;
+        let mut reposonse = Vec::<AnimalTypeSearchResponse>::new();
+        match result {
+            Ok(r) => {
+                for elem in r {
+                    reposonse.push(elem.into());
+                }
+            }
+            Err(e) => {
+                tracing::warn!("can not fonud data : {:#?}", e);
+            }
+        }
+        reposonse
     }
 
     async fn modfiy_animal_type(
