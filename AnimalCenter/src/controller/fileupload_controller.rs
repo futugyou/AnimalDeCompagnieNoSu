470f02b576bca1d8ed8c4d60fc0b92b821ba6883
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-02-22 17:31:40 +0000

    Update fileupload_controller.rs
    test store the Bytes to mongodb

--- AnimalCenter/src/controller/fileupload_controller.rs
+++ AnimalCenter/src/controller/fileupload_controller.rs
@@ -2,9 +2,15 @@ use std::io::Write;
 
 use actix_multipart::Multipart;
 use actix_web::{web, Error, HttpResponse};
+use bson::{doc, spec::BinarySubtype, Binary, Bson};
 use futures::{StreamExt, TryStreamExt};
 
+use crate::infrastruct::context::dbcontext::{DBContext, IDbContext};
+
 pub async fn post(mut payload: Multipart) -> Result<HttpResponse, Error> {
+    let dbcontext = DBContext {};
+    let dbclient = dbcontext.get_db_context().await.unwrap();
+    let collection = dbclient.database("react-app").collection("upload");
     // iterate over multipart stream
     while let Ok(Some(mut field)) = payload.try_next().await {
         let content_type = field.content_disposition().unwrap();
@@ -12,16 +18,28 @@ pub async fn post(mut payload: Multipart) -> Result<HttpResponse, Error> {
         let filepath = format!("{}", sanitize_filename::sanitize(&filename));
         println!("filepath: {:?}", filepath);
         // File::create is blocking operation, use threadpool
-        let mut f = web::block(|| std::fs::File::create(filepath))
-            .await
-            .unwrap();
-
+        // let mut f = web::block(|| std::fs::File::create(filepath))
+        //     .await
+        //     .unwrap();
+        let mut file_data = Vec::<u8>::new();
         // Field in turn is stream of *Bytes* object
         while let Some(chunk) = field.next().await {
             let data = chunk.unwrap();
+            file_data.extend_from_slice(&data);
             // filesystem operations are blocking, we have to use threadpool
-            f = web::block(move || f.write_all(&data).map(|_| f)).await?;
+            //f = web::block(move || f.write_all(&data).map(|_| f)).await?;
         }
+        //web::block(move || f.write_all(&file_data).map(|_| f)).await?;
+        let binary = Bson::Binary(Binary {
+            subtype: BinarySubtype::Generic,
+            bytes: file_data,
+        });
+        let docs = doc! {
+                "name":binary,
+        };
+        let rr = collection.insert_one(docs, None).await.unwrap();
+        println!("----{:#?}", rr);
     }
+
     Ok(HttpResponse::Ok().into())
 }
