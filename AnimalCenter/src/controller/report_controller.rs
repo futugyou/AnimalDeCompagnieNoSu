084a047bbef026565e53ad115ab2e1dc153b5eb9
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-03-18 19:24:02 +0000

    change paramter from body to url
    change paramter from body to url, c# can not send httpget with body

--- AnimalCenter/src/controller/report_controller.rs
+++ AnimalCenter/src/controller/report_controller.rs
@@ -1,17 +1,13 @@
 use crate::model::report::rescue_classification::RescueClassificationRequest;
 use crate::service::reportservice::*;
-use actix_web::{web, HttpRequest, HttpResponse};
+use actix_web::{web::Query, HttpRequest, HttpResponse};
 
 pub async fn get_rescue_classification(
-    item: Option<web::Json<RescueClassificationRequest>>,
+    Query(classic): Query<RescueClassificationRequest>,
+    //item: Option<web::Json<RescueClassificationRequest>>,
     _req: HttpRequest,
 ) -> HttpResponse {
-    let mut paramter = RescueClassificationRequest::default();
-    match item {
-        Some(r) => paramter = r.into_inner(),
-        None => (),
-    }
     let service = ReportService::new().await;
-    let result = service.get_rescue_classification(paramter).await;
+    let result = service.get_rescue_classification(classic).await;
     HttpResponse::Ok().json(result)
 }
