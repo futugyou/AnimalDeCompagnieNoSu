d69595780009471c0e62ebb5c16f0cd99166739c
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-03-22 17:33:13 +0000

    move upload logic from controller to service
    move upload logic from controller to service

--- AnimalCenter/src/controller/staticfile_controller.rs
+++ AnimalCenter/src/controller/staticfile_controller.rs
@@ -1,56 +1,32 @@
-use crate::infrastruct::config::Config;
-use crate::model::file::staticfilemodel::StaticfileResponse;
-use async_std::fs::{self, File};
-use async_std::prelude::*;
-use chrono::Utc;
-use rand::Rng;
-use std::path::Path;
+use crate::{
+    infrastruct::custom_error::CustomError, service::staticfileservice::StaticfileService,
+};
+use crate::{
+    model::file::staticfilemodel::{StaticfileRequest, StaticfileResponse},
+    service::staticfileservice::IStaticfileService,
+};
 
 use actix_multipart::Multipart;
-use actix_web::{Error, HttpResponse};
+use actix_web::{web::Bytes, HttpResponse};
 use futures::{StreamExt, TryStreamExt};
 
-pub async fn post(mut payload: Multipart) -> Result<HttpResponse, Error> {
-    let mut response = Vec::<StaticfileResponse>::new();
-
-    let config = Config::new();
-    let address = config.server_address;
-
-    let subfilepath = Utc::now().format("%Y-%m-%d").to_string();
-    let access_address = format!("http://{}/{}/{}", address, "images", subfilepath);
-
-    let path_string = format!("{}{}", "fileupload/", subfilepath);
-    let path = Path::new(&path_string);
-    fs::create_dir_all(path).await?;
-
+pub async fn post(mut payload: Multipart) -> Result<HttpResponse, CustomError> {
+    let staticserver = StaticfileService {};
+    let mut response = StaticfileResponse::default();
     // iterate over multipart stream
+    // Upload "one" file at a time
     while let Ok(Some(mut field)) = payload.try_next().await {
         let content_type = field.content_disposition().unwrap();
         let filename = content_type.get_filename().unwrap();
-        let mut filename = sanitize_filename::sanitize(filename);
-        filename = format!(
-            "{}-{:>04}-{}",
-            Utc::now().format("%Y%m%d-%H%M%S"),
-            rand::thread_rng().gen_range(0001..9999),
-            filename
-        );
-        let access_address = format!("{}/{}", access_address, filename);
-        let filepath = format!("{}/{}", path_string, filename);
-
-        let mut f = File::create(filepath.clone()).await?;
+        let filename = sanitize_filename::sanitize(filename);
+        let mut filedata = Vec::<Bytes>::new();
         // Field in turn is stream of *Bytes* object
         while let Some(chunk) = field.next().await {
             let data = chunk.unwrap();
-            f.write_all(&data).await?;
+            filedata.push(data);
         }
-        f.flush().await?;
-        let staticfile = StaticfileResponse {
-            name: filename.clone(),
-            status: "done".to_owned(),
-            thumb_url: format!("{}", access_address),
-            url: format!("{}", access_address),
-        };
-        response.push(staticfile);
+        let request = StaticfileRequest { filename, filedata };
+        response = staticserver.savestaticfile(request).await?;
     }
-    Ok(HttpResponse::Ok().json(response))
+    return Ok(HttpResponse::Ok().json(response));
 }
