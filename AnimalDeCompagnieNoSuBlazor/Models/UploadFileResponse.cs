1edb79b02d95c145e98f8775da0d78f6aadb90f6
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-03-23 16:08:26 +0000

    add upload image response
    add upload image response

---
+++ AnimalDeCompagnieNoSuBlazor/Models/UploadFileResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalDeCompagnieNoSuBlazor.Models
+{
+    public class ResponseModel
+    {
+        public string name { get; set; }
+
+        public string status { get; set; }
+
+        public string url { get; set; }
+
+        public string thumbUrl { get; set; }
+    }
+}
--- AnimalDeCompagnieNoSuBlazor/Pages/Animal/AnimalModify/AnimalModifyStep2.razor.cs
+++ AnimalDeCompagnieNoSuBlazor/Pages/Animal/AnimalModify/AnimalModifyStep2.razor.cs
@@ -137,16 +137,5 @@ namespace AnimalDeCompagnieNoSuBlazor.Pages.Animal
                 }
             }
         }
-
-        public class ResponseModel
-        {
-            public string name { get; set; }
-
-            public string status { get; set; }
-
-            public string url { get; set; }
-
-            public string thumbUrl { get; set; }
-        }
     }
 }
