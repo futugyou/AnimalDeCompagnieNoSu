da366545118b013b7d31cd90cd855863a5497cf8
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-03-23 15:41:16 +0000

    add JsonPropertyName
    add JsonPropertyName

--- AnimalDeCompagnieNoSuBlazor/Models/AnimalListViewModel.cs
+++ AnimalDeCompagnieNoSuBlazor/Models/AnimalListViewModel.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace AnimalDeCompagnieNoSuBlazor.Models
 {
     public class AnimalListViewModel
     {
-        public int Id { get; set; }
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("type")]
         public string Type { get; set; }
+        [JsonPropertyName("sub_type")]
         public string SubType { get; set; }
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+        [JsonPropertyName("short_describe")]
         public string ShortDescribe { get; set; }
+        [JsonPropertyName("avatar")]
         public string Photoe { get; set; }
     }
 }
--- AnimalDeCompagnieNoSuBlazor/Models/AnimalViewModel.cs
+++ AnimalDeCompagnieNoSuBlazor/Models/AnimalViewModel.cs
@@ -1,21 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace AnimalDeCompagnieNoSuBlazor.Models
 {
     public class AnimalViewModel
     {
-        public int Id { get; set; }
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("idcard")]
         public string Idcard { get; set; }
+        [JsonPropertyName("type")]
         public string Type { get; set; }
+        [JsonPropertyName("sub_type")]
         public string SubType { get; set; }
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+        [JsonPropertyName("avatar")]
         public string Avatar { get; set; }
+        [JsonPropertyName("age")]
         public double Age { get; set; }
+        [JsonPropertyName("short_describe")]
         public string ShortDescribe { get; set; }
+        [JsonPropertyName("photoes")]
         public List<string> Photoes { get; set; } = new List<string>();
+        [JsonPropertyName("birthday")]
         public DateTime? Birthday { get; set; }
     }
 }
