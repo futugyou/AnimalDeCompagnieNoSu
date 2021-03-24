5eda436f3e3e09ad71567e0470a0304b4853ffcd
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-03-24 18:25:33 +0000

    Update AnimalUpdateModel.cs
    add animal image upload model

--- AnimalDeCompagnieNoSuBlazor/Models/AnimalUpdateModel.cs
+++ AnimalDeCompagnieNoSuBlazor/Models/AnimalUpdateModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace AnimalDeCompagnieNoSuBlazor.Models
@@ -18,5 +19,19 @@ namespace AnimalDeCompagnieNoSuBlazor.Models
         public string ShortDescribe { get; set; }
         public List<string> Photoes { get; set; } = new List<string>();
         public DateTime? Birthday { get; set; }
+    }
+
+    public class AnimalAvatarUploadModel
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("avatar")]
+        public string Avatar { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+        [JsonPropertyName("sub_type")]
+        public string SubType { get; set; }
     }
 }
