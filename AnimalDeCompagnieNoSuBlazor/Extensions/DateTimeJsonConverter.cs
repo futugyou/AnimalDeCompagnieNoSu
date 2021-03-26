441b6fa981f613f5c1b37d11b5f8774c70cbf39d
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-03-26 19:57:58 +0000

    add datetime json convert
    add datetime json convert to format datetime with  "yyyy-MM-ddTHH:mm:ssZ"

---
+++ AnimalDeCompagnieNoSuBlazor/Extensions/DateTimeJsonConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AnimalDeCompagnieNoSuBlazor.Extensions
+{
+    public class DateTimeJsonConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return DateTime.ParseExact(reader.GetString(),
+                    "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(
+                       "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture));
+        }
+    }
+}
