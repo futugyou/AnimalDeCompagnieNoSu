194bf6a469978960ba675decea46bd4e557e48cc
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-03-28 18:55:58 +0000

    add string to vec deserialize
    add string to vec deserialize
    like 1,2,3->[1,2,3]

--- AnimalCenter/src/infrastruct/serialize/mod.rs
+++ AnimalCenter/src/infrastruct/serialize/mod.rs
@@ -5,8 +5,11 @@ pub mod naive_date_format;
 #[allow(dead_code)]
 pub mod naive_date_format_bson;
 
-use serde::{de, Deserialize, Deserializer};
-use std::fmt::Display;
+use serde::{
+    de::{self, IntoDeserializer},
+    Deserialize, Deserializer,
+};
+use std::fmt::{self, Display};
 use std::str::FromStr;
 
 pub fn deserialize_object_id<'de, S, D>(deserializer: D) -> Result<S, D::Error>
@@ -24,4 +27,39 @@ where
         Err(_) => (),
     };
     S::from_str(&str).map_err(de::Error::custom)
+}
+
+pub fn deserialize_stringified_list<'de, D, I>(
+    deserializer: D,
+) -> std::result::Result<Vec<I>, D::Error>
+where
+    D: de::Deserializer<'de>,
+    I: de::DeserializeOwned,
+{
+    struct StringVecVisitor<I>(std::marker::PhantomData<I>);
+
+    impl<'de, I> de::Visitor<'de> for StringVecVisitor<I>
+    where
+        I: de::DeserializeOwned,
+    {
+        type Value = Vec<I>;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a string containing a list")
+        }
+
+        fn visit_str<E>(self, v: &str) -> std::result::Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            let mut ids = Vec::new();
+            for id in v.split(",") {
+                let id = I::deserialize(id.into_deserializer())?;
+                ids.push(id);
+            }
+            Ok(ids)
+        }
+    }
+
+    deserializer.deserialize_any(StringVecVisitor(std::marker::PhantomData::<I>))
 }
