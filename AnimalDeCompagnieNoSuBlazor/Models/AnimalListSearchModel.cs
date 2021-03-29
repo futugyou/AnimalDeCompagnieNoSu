faeeb5133aa1f000736d885efbc5d01c494bcb78
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-03-29 16:55:57 +0000

    add animal search model
    add animal search model

---
+++ AnimalDeCompagnieNoSuBlazor/Models/AnimalListSearchModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AnimalDeCompagnieNoSuBlazor.Models
+{
+    public class AnimalListSearchModel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+        public override string ToString()
+        {
+            return string.Format($"?name={Name}&type={Type}");
+        }
+    }
+}
