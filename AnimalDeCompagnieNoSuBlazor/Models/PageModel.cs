e6f3ebdfb984cff9714823089fab1034db0752b8
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-03-31 15:08:05 +0000

    Create PageModel.cs
    add pagemodel and pageviewmodel

---
+++ AnimalDeCompagnieNoSuBlazor/Models/PageModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AnimalDeCompagnieNoSuBlazor.Models
+{
+    public class PageModel
+    {
+        [JsonPropertyName("pagesize")]
+        public int PageSize { get; set; } = 8;
+        [JsonPropertyName("pageindex")]
+        public int PageIndex { get; set; } = 1;
+
+        public override string ToString()
+        {
+            return $"pagesize={PageSize}&pageindex={PageIndex}";
+        }
+    }
+
+    public class PageViewModel
+    {
+        public int PageIndex { get; set; } = 1;
+        public int PageSize { get; set; } = 8;
+        public int TotalCount { get; set; } = 50;
+    }
+}
