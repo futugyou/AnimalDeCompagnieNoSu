8313183b6ab62dcd9758b2e70251fd841480b350
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-03-31 10:27:18 +0000

    add pagemodel
    add pagemodel

--- AnimalCenter/src/model/mod.rs
+++ AnimalCenter/src/model/mod.rs
@@ -1,4 +1,15 @@
+use crate::infrastruct::serialize::from_str;
+use serde::{Deserialize, Serialize};
+
 pub mod animal;
 pub mod animaltype;
 pub mod file;
 pub mod report;
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct PageModel {
+    #[serde(deserialize_with = "from_str")]
+    pub pageindex: u64,
+    #[serde(deserialize_with = "from_str")]
+    pub pagesize: u64,
+}
