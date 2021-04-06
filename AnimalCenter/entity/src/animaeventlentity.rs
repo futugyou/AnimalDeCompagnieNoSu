4a2f8e6a9064ddfc269094c714e8d4e73530188b
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-04-06 15:52:41 +0000

    impl new and get_collection_name for AnimalEventEntity
    impl new and get_collection_name for AnimalEventEntity

--- AnimalCenter/entity/src/animaeventlentity.rs
+++ AnimalCenter/entity/src/animaeventlentity.rs
@@ -5,7 +5,7 @@ use bson::{doc, Document};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Default, Serialize, Deserialize, Clone)]
 pub struct AnimalEventEntity {
     #[serde(alias = "_id", default, deserialize_with = "deserialize_object_id")]
     pub id: String,
@@ -17,4 +17,14 @@ pub struct AnimalEventEntity {
     pub event: String,
     #[serde(with = "date_format_bson", default)]
     pub event_time: Option<DateTime<Utc>>,
+}
+
+impl AnimalEventEntity {
+    pub fn new() -> Self {
+        AnimalEventEntity::default()
+    }
+
+    pub fn get_collection_name() -> &'static str {
+        "animal-event"
+    }
 }
