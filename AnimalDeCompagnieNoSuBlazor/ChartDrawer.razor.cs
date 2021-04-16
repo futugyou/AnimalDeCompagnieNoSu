c287cdd1642421a2c327f27c6af5f332ae16b970
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-04-16 09:13:05 +0000

    add chart drawer skeleton
    add chart drawer skeleton

---
+++ AnimalDeCompagnieNoSuBlazor/ChartDrawer.razor.cs
@@ -0,0 +1,25 @@
+﻿using AntDesign;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalDeCompagnieNoSuBlazor
+{
+    public partial class ChartDrawer : AntDomComponentBase
+    {
+        private bool _show;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+        }
+        private void SetShow(MouseEventArgs args)
+        {
+            _show = !_show;
+        }
+
+    }
+}
