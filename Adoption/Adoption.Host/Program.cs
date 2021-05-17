ceef5a06b2e644fae2e1479cd212083c83ada37a
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-05-17 11:09:35 +0800

    Add serilog with GrafanaLoki

--- Adoption/Adoption.Host/Program.cs
+++ Adoption/Adoption.Host/Program.cs
@@ -1,16 +1,33 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Grafana.Loki;
+using System;
+using System.IO;
 
 namespace Adoption.Host
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args)
-            .Build()
-            //.InitializeDbContexts()
-            .Run();
+            try
+            {
+                Log.Information("Starting web host.");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly!");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
@@ -21,8 +38,31 @@ namespace Adoption.Host
                 {
                     webBuilder.UseStartup<Startup>();
                 })
-                .UseAutofac();
+                .UseAutofac()
+                .UseSerilog(InitSerilog);
             return builder;
         }
+
+        private static readonly Action<HostBuilderContext, LoggerConfiguration> InitSerilog = (context, config) =>
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            config
+            .Enrich.FromLogContext()
+#if DEBUG
+            .MinimumLevel.Debug()
+#else
+            .MinimumLevel.Information()
+#endif
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+            .Enrich.WithProperty("app", context.HostingEnvironment.ApplicationName)
+            .WriteTo.GrafanaLoki(configuration["GrafanaLoki:Uri"])
+            .WriteTo.Console();
+        };
     }
 }
