fe7344767fe5ab145cf239a8bef29d623be3a9e3
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-06-12 16:46:32 +0800

    change bind port and update Dockerfile

--- AnimalCenter/web/src/main.rs
+++ AnimalCenter/web/src/main.rs
@@ -45,7 +45,7 @@ async fn main() -> std::io::Result<()> {
         .service(route_graphql::graphqlscope())
         // #endregion
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
