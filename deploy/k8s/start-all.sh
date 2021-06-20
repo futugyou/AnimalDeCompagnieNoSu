8f0c7c2c36d04fddaccd46eb4c5769ced2f689de
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-06-20 19:19:13 +0800

    update deploy file

--- deploy/k8s/start-all.sh
+++ deploy/k8s/start-all.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-kubectl apply \
-    -f ./namespace.yaml \
-    -f ./sqldata.yaml \
-    -f ./adoption.yaml
+# namespace
+kubectl apply -f ./namespace.yaml
+
+# grafana-loki
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+helm upgrade --install loki grafana/loki-stack --namespace=anima --set grafana.enabled=true,prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false
+
+# db 
+kubectl apply -f ./sqldata.yaml
+
+# mq
+kubectl apply -f ./rabbitmq.yaml
+
+# service
+kubectl apply -f ./adoption.yaml
+kubectl apply -f ./animal-center.yaml
