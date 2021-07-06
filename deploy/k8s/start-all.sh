09f5f48d1c7e48071add4d9efa52612190426715
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-07-06 15:47:13 +0800

    update skaffold.yaml

--- deploy/k8s/start-all.sh
+++ deploy/k8s/start-all.sh
@@ -6,7 +6,7 @@ kubectl apply -f ./namespace.yaml
 # grafana-loki
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-helm upgrade --install loki grafana/loki-stack --namespace=anima --set grafana.enabled=true,prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false
+helm upgrade --install loki grafana/loki-stack --namespace=animal --set grafana.enabled=true,prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false
 
 # db 
 kubectl apply -f ./sqldata.yaml
