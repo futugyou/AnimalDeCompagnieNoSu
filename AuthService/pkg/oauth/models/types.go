74fdeb7e5a4ef27eee5c3ee836b1453a4e0bd229
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-08-13 09:53:11 +0800

    add client type

---
+++ AuthService/pkg/oauth/models/types.go
@@ -0,0 +1,28 @@
+package models
+
+type Client struct {
+	ID     string   `json:"id,omitempty"`
+	Secret string   `json:"secret,omitempty"`
+	Domain string   `json:"domain,omitempty"`
+	UserID string   `json:"userID,omitempty"`
+	Scopes []string `json:"scopes,omitempty"`
+}
+
+func (c *Client) GetID() string {
+	return c.ID
+}
+
+func (c *Client) GetSecret() string {
+	return c.Secret
+}
+
+func (c *Client) GetDomain() string {
+	return c.Domain
+}
+
+func (c *Client) GetUserID() string {
+	return c.UserID
+}
+func (c *Client) GetScopes() []string {
+	return c.Scopes
+}
