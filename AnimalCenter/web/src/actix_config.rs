2906d01a4b80dcada618f29163c672105dcb7ce9
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-08-08 01:49:10 +0000

    add 'metrics' endpoint to show metrics

--- AnimalCenter/web/src/actix_config.rs
+++ AnimalCenter/web/src/actix_config.rs
@@ -1,7 +1,8 @@
 use actix_web::web::ServiceConfig;
-use actix_web::{HttpResponse, Responder};
-use actix_web_lab::middleware::from_fn;
+use actix_web::{get, HttpResponse, Responder};
+use actix_web_lab::{extract::ThinData, middleware::from_fn};
 use dotenv::dotenv;
+use metrics_exporter_prometheus::PrometheusHandle;
 
 use crate::observable;
 use crate::route::{crudroute, route_graphql};
@@ -14,8 +15,18 @@ pub fn create_config(cfg: &mut ServiceConfig) {
     dotenv().ok();
 
     observable::logging::init();
+    let handle = observable::prometheus::init();
+
+    cfg.app_data(ThinData(handle.clone()));
 
     cfg.route("/healthz", actix_web::web::get().to(health_check));
+    cfg.service(metrics);
+
     cfg.service(crudroute::bussisscope().wrap(from_fn(observable::middleware::request_telemetry)));
     cfg.service(route_graphql::graphqlscope());
+}
+
+#[get("/metrics")]
+pub(crate) async fn metrics(metrics_handle: ThinData<PrometheusHandle>) -> impl Responder {
+    metrics_handle.render()
 }
--- AnimalCenter/web/src/observable/mod.rs
+++ AnimalCenter/web/src/observable/mod.rs
@@ -1,3 +1,4 @@
 pub mod logging;
 mod metric_names;
 pub mod middleware;
+pub mod prometheus;
